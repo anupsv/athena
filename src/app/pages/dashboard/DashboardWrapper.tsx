e257d089c53890b9b9f0fa4c6e68b015b57f2c46
asv <asv@asvs-MacBook-Pro.local>
2022-11-19 08:02:31 -0800

    adding activity

--- src/app/pages/dashboard/DashboardWrapper.tsx
+++ src/app/pages/dashboard/DashboardWrapper.tsx
@@ -1,21 +1,9 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import {FC} from 'react'
 import {useIntl} from 'react-intl'
-import {toAbsoluteUrl} from '../../../_metronic/helpers'
 import {PageTitle} from '../../../_metronic/layout/core'
 import {
-  ListsWidget2,
-  ListsWidget3,
-  ListsWidget4,
-  ListsWidget6,
-  TablesWidget5,
-  TablesWidget10,
-  MixedWidget8,
-  CardsWidget7,
-  CardsWidget17,
-  CardsWidget20,
-  ListsWidget26,
-  EngageWidget10,
+    ListsWidget5,
 } from '../../../_metronic/partials/widgets'
 
 const DashboardPage: FC = () => (
@@ -23,7 +11,8 @@ const DashboardPage: FC = () => (
 
     <div className='row g-5 gx-xxl-12'>
       <div className='col-xxl-11'>
-        <TablesWidget5 className='card-xxl-stretch mb-5 mb-xxl-8' />
+        <ListsWidget5 className='card-xxl-stretch mb-5 mb-xxl-8' />
+        {/*<TablesWidget5 className='card-xxl-stretch mb-5 mb-xxl-8' />*/}
       </div>
     </div>
   </>
